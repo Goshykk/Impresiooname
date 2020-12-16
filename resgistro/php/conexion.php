cb2d0f15dfa719dc557f8ae5313e248b38e2e867
Jonathan Juarez Villagomez <goshikkjjv@gmail.com>
2020-12-16 12:25:54 -0600

    update

--- resgistro/php/conexion.php
+++ resgistro/php/conexion.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost:3306";
 $usuario = "root";
-$clave = "#Lovegame19#";
+$clave = "#Love19#";
 $bd = "registro";
 
 $conexion = mysqli_connect($host, $usuario, $clave, $bd, 3306);
